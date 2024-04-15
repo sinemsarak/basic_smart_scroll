7e7ade21317725ef942bf569d78417bdfb725654
Sinem Sarak <snmsrkss@gmail.com>
2024-04-15 16:42:44 +0300

    login and homepage merged

--- lib/circle.dart
+++ lib/circle.dart
@@ -7,16 +7,16 @@ class MyCircle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-              height: 100, 
-              width: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.purple[200],
-              ),
-              child: Center(child: Text(child, style: TextStyle(fontSize: 20))),
-              ),
-              );
-           }      
-         }
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.purple[200],
+        ),
+        child: Center(child: Text(child, style: TextStyle(fontSize: 20))),
+      ),
+    );
+  }
+}
---
+++ lib/splash_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:smart_scroll_hw/login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future<void>.delayed(const Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/splashScreen.png"),
+              fit: BoxFit.cover)),
+    );
+  }
+}
